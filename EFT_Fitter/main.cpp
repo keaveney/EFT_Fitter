de22cb39f371afe87e652b4069646702b88abdb4
James Keaveney <james.keaveney@desy.de>
2017-01-19 11:42:30 +0100

    Initial Commit

---
+++ EFT_Fitter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EFT_Fitter
+//
+//  Created by James Keaveney on 19/01/2017.
+//  Copyright © 2017 James Keaveney. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
